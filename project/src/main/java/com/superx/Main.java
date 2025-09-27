be26c86ab8a4ccc6624bf182283db6d46942fe2a
AtharvaMore1903 <moreatharva193@gmail.com>
2025-09-27 21:40:56 +0530

    Done

---
+++ project/src/main/java/com/superx/Main.java
@@ -0,0 +1,17 @@
+package com.superx;
+
+import java.util.concurrent.ExecutionException;
+
+import com.superx.Configuration.FireBaseInitializer;
+import com.superx.View.LoginPage;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        System.out.println("Hello Couponomy");
+        FireBaseInitializer fireBaseInitializer = new FireBaseInitializer();
+        Application.launch(LoginPage.class, args);
+    }
+}
